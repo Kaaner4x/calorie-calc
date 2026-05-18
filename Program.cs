2dc8bcde45ccbb908505038585385db37e7ab3dc
Kaaner4x <kaaner.emirhan@gmail.com>
2026-05-19 01:34:03 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalorieCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
